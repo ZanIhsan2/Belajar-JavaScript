2cc9addc1df82db1d1e9deeb96243ed5f39b0e0a
ZanIhsan2 <rasyid1234rismawan@gmail.com>
2025-07-19 23:56:58 +0700

    js_fundamental

---
+++ JavaScript-Fundamental/fundamental.js
@@ -0,0 +1,39 @@
+// =======================
+// 🔸 Komentar di JavaScript
+// =======================
+
+// Ini adalah komentar satu baris
+
+/*
+Ini adalah komentar banyak baris.
+Biasa digunakan untuk menjelaskan bagian kode lebih detail.
+*/
+
+// =======================
+// 🔸 Tipe Data di JavaScript
+// =======================
+
+let nama = "Razan"; // String (pakai single quote)
+let kalimat1 = "Saya bapaknya Razan"; // String (pakai double quote)
+let umur = 18; // Number (bilangan bulat)
+let tinggiBadan = 170.2; // Number (bilangan desimal)
+let sudahMakan = true; // Boolean (true/false)
+let nilai = [75, 70, 85, 92]; // Array (kumpulan nilai)
+
+let user = {
+  // Object (data terstruktur: key dan value)
+  name: "Razan",
+  age: 18,
+  tinggiBadan: 170.2,
+};
+
+// =======================
+// 🔸 Operator di JavaScript
+// =======================
+
+let saya = "Razan";
+let age = 18;
+
+let kalimat = "Halo, saya " + saya + " dan umur saya " + age + " tahun.";
+
+console.log(kalimat); // Output: Halo, saya Razan dan umur saya 18 tahun.
